4a748b766d1d61b5497c9ac5a7e152f023b5c8d2
m724tmy <tomoya.724.a7x@icloud.com>
2024-09-24 17:59:06 +0900

    PW英数字含めたバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # validates :
+  validates :name, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+
+  # パスワードのバリデーション設定
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'には英字と数字の両方を含めてください' }
 end
