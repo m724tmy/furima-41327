aead005dd8c2a28c3f168c862fb8ebf290c1ed4a
m724tmy <tomoya.724.a7x@icloud.com>
2024-09-29 19:42:26 +0900

    migrate file

--- db/migrate/20240929101228_create_items.rb
+++ db/migrate/20240929101228_create_items.rb
@@ -3,6 +3,15 @@ class CreateItems < ActiveRecord::Migration[7.0]
     create_table :items do |t|
 
       t.timestamps
+      t.references :user,            null: false, foreign_key: true
+      t.string     :name,            null: false
+      t.text       :description,     null: false
+      t.integer    :category_id,     null: false
+      t.integer    :status_id,       null: false
+      t.integer    :shipping_fee_id, null: false
+      t.integer    :prefecture_id  , null: false
+      t.integer    :shipping_day_id, null: false
+      t.integer    :price,           null: false
     end
   end
 end
