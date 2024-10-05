5a334eb4bfcc9cfc51da035009e2f1975a4ef454
m724tmy <tomoya.724.a7x@icloud.com>
2024-10-05 15:29:31 +0900

    ActiveStorage

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  # ヴァリデーション設定
+  validates :name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price,
+            presence: true
 end
