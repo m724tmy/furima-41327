c6c990c98fa106594e2f4fecf3219fdaad97d4b9
m724tmy <tomoya.724.a7x@icloud.com>
2024-10-05 15:08:39 +0900

    ActiveStorage実装準備
    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,21 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
+                                 :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
