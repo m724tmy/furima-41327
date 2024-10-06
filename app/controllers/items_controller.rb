4a2781f2fc2de67c882f7c7c81f09f1845578666
m724tmy <tomoya.724.a7x@icloud.com>
2024-10-06 22:57:00 +0900

    出品機能（DB保存）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,14 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
     redirect_to '/'
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
-                                 :shipping_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(
+      :image, :name, :description, :category_id, :status_id,
+      :shipping_fee_id, :prefecture_id, :shipping_day_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
