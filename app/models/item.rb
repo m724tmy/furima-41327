c55f878b2bb7497a3e0b753d547018c98f03ca21
m724tmy <tomoya.724.a7x@icloud.com>
2024-10-09 21:05:28 +0900

    モデル単体テスト実装準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  validates :name, :description, :price, presence: true
+  validates :image, :name, :description, presence: true
   validates :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Commerce.product_name }    # ランダムな商品名を生成
+    description { Faker::Lorem.paragraph }   # ランダムな段落を生成
+    category_id { Faker::Number.between(from: 2, to: 10) }  # ランダムなカテゴリーID（1以外）を生成
+    status_id { Faker::Number.between(from: 2, to: 5) }     # ランダムなステータスID
+    shipping_fee_id { Faker::Number.between(from: 2, to: 5) } # ランダムな送料ID
+    prefecture_id { Faker::Number.between(from: 2, to: 47) }  # ランダムな都道府県ID
+    shipping_day_id { Faker::Number.between(from: 2, to: 5) } # ランダムな発送日数ID
+    price { Faker::Number.between(from: 300, to: 9_999_999) } # ランダムな価格（¥300〜¥9,999,999）v
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
