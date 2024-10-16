6876e005202ebc7bdfb6fee2f5a3ab8a5e062760
m724tmy <tomoya.724.a7x@icloud.com>
2024-10-16 21:14:03 +0900

    エラーハンドリングの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,15 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
