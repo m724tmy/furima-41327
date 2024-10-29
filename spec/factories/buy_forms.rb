877c2c1782fd2864a71df6dc8106219fd91ff3ed
m724tmy <tomoya.724.a7x@icloud.com>
2024-10-29 20:34:59 +0900

    factoryアソシエーションの削除

--- spec/factories/buy_forms.rb
+++ spec/factories/buy_forms.rb
@@ -7,7 +7,5 @@ FactoryBot.define do
       building_name { '渋谷ビル' }
       phone_number { '09012345678' }
       token {"tok_abcdefghijk00000000000000000"}
-      association :user_id
-      association :item_id
     end
   end
